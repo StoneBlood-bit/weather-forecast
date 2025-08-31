8f30556a12ffd05f2f4ae4dd0a536ecae9af840e
StoneBlood <brkalix@gmail.com>
2025-08-31 12:38:28 +0300

    created the method `getForecastSafely`

--- src/main/java/weatherforecast/service/WeatherService.java
+++ src/main/java/weatherforecast/service/WeatherService.java
@@ -19,29 +19,42 @@ public class WeatherService {
     private final ObjectMapper objectMapper = new ObjectMapper();
 
     public WeatherForecast getForecast(String city) {
-        String apiKey = dotenv.get("API_KEY");
+        return getForecastSafely(city);
+    }
+
+    private WeatherForecast getForecastSafely(String city) {
         try {
+            String apiKey = dotenv.get("API_KEY");
             String url = URL + "?key=" + apiKey + "&q=" + city + "&days=2&aqi=no&alerts=no";
+
             HttpRequest request = HttpRequest.newBuilder()
                     .uri(URI.create(url))
                     .build();
+
             HttpResponse<String> response = httpClient.send(request, HttpResponse.BodyHandlers.ofString());
-
             JsonNode root = objectMapper.readTree(response.body());
-
             JsonNode forecastTomorrow = root.path("forecast").path("forecastday").get(1).path("day");
 
             WeatherForecast forecast = new WeatherForecast();
             forecast.setCity(city);
-            forecast.setDate(root.path("forecast").path("forecastday").get(1).path("date").asText());
-            forecast.setMaxTempC(forecastTomorrow.path("maxtemp_c").asDouble());
-            forecast.setMinTempC(forecastTomorrow.path("mintemp_c").asDouble());
-            forecast.setHumidity(forecastTomorrow.path("avghumidity").asInt());
-            forecast.setWindKph(forecastTomorrow.path("maxwind_kph").asDouble());
-            forecast.setWindDir(forecastTomorrow.path("condition").path("text").asText());
+
+            forecast.setDate(safeGet(() -> root
+                    .path("forecast").path("forecastday").get(1).path("date").asText(), "N/A"));
+            forecast.setMaxTempC(safeGet(() -> forecastTomorrow
+                    .path("maxtemp_c").asDouble(), 0.0));
+            forecast.setMinTempC(safeGet(() -> forecastTomorrow
+                    .path("mintemp_c").asDouble(), 0.0));
+            forecast.setHumidity(safeGet(() -> forecastTomorrow
+                    .path("avghumidity").asInt(), 0));
+            forecast.setWindKph(safeGet(() -> forecastTomorrow
+                    .path("maxwind_kph").asDouble(), 0.0));
+            forecast.setWindDir(safeGet(() -> forecastTomorrow
+                    .path("condition").path("text").asText(), "Not loaded")
+            );
+
             return forecast;
         } catch (Exception e) {
-            throw new ForecastException("Failed to fetch forecast for city: " + city);
+            throw new ForecastException("Failed to fetch forecast for city: " + city, e);
         }
     }
 
